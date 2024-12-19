c3d87c168a1fa7871486735cfe254f8ff74150c8
Fucci <benjaminakacha@gmail.com>
2024-12-18 21:39:44 -0800

    solve: SOLVED THE SODUKU code/ deleted useless files!

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -14,7 +14,6 @@ class SudokuSolver {
       I: 72,
     };
     this.tree = this.buildGraph();
-
   }
   buildGraph() {
     let graph = [];
@@ -29,33 +28,33 @@ class SudokuSolver {
     return puzzleString.length === 81 && /^[1-9.]+$/.test(puzzleString);
   }
   checkRowPlacement(puzzleString, row, column, value) {
-    let rowStart = this.rowValue[row]
-    for (let i=rowStart; i<rowStart+9;i++){
-      if(puzzleString[i]==value)  return false
+    let rowStart = this.rowValue[row];
+    for (let i = rowStart; i < rowStart + 9; i++) {
+      if (puzzleString[i] == value) return false;
     }
-    return true
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-    let colStart = column-1
-    for(let i = colStart; i<81;i+=9){
-        if(puzzleString[i]==value)return false
+    let colStart = column - 1;
+    for (let i = colStart; i < 81; i += 9) {
+      if (puzzleString[i] == value) return false;
     }
-    return true
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
     let colStart = Math.floor((column - 1) / 3) * 3;
-    let rowStart = Math.floor((this.rowValue[row]) /27)*27
-   for(let c=0;c<3;c++){
-    for(let r=0; r<3;r++){
-      let scanIndex = colStart + rowStart + r + c*9
-      if(puzzleString[scanIndex]==value){
-        return false
+    let rowStart = Math.floor(this.rowValue[row] / 27) * 27;
+    for (let c = 0; c < 3; c++) {
+      for (let r = 0; r < 3; r++) {
+        let scanIndex = colStart + rowStart + r + c * 9;
+        if (puzzleString[scanIndex] == value) {
+          return false;
+        }
       }
     }
-   }
-   return true
+    return true;
   }
   isValidPlacement(puzzleString, row, column, value) {
     return (
@@ -65,12 +64,29 @@ class SudokuSolver {
     );
   }
   solve(puzzleString) {
-    let problem = puzzleString
-    for (let i =0; i<problem.length;i++){
-      if(problem[i]=="."){
-        
+    let problem = puzzleString;
+  
+ 
+    for (let i = 0; i < problem.length; i++) {
+      if (problem[i] == ".") {
+        for (let testNumber = 1; testNumber <= 9; testNumber++) {
+          if (
+            this.isValidPlacement(
+              problem,
+              this.tree[i][0],
+              this.tree[i][1],
+              testNumber
+            )
+          ) {
+            problem.splice(i, 1, testNumber);
+            if(this.solve(problem))return problem
+            problem[i] = "."
+          }
+        }
+        return false
       }
     }
+    return problem
   }
 }
 
